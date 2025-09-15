ffb7a2b72dadb734d01ff56b49ba61e2009088a8
Prashant Choudhary <93462306+Prash-Java@users.noreply.github.com>
2025-09-15 22:10:17 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,14 @@
+str1 = "Java"
+str2 = 'Python'
+str3 = """Kotlin"""
+print(str1)
+print(str2)
+print(str3)
+str4 = str1 + " " + str3
+print(str4)
+print("This is Java. This is Python. This is Kotlin.")
+print("This is Java.\nThis is Python.\tThis is Kotlin.")
+print(len(str1))
+print(len(str2))
+print(len(str3))
+print(len(str4))
