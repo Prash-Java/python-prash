34e9bbe3c697d2d6be189341ae69d5e1272659fd
Prashant Choudhary <93462306+Prash-Java@users.noreply.github.com>
2025-09-27 09:58:46 +0530

    Add files via upload

--- WhileLoopsWithBreak.py
+++ WhileLoopsWithBreak.py
@@ -6,3 +6,31 @@ while k <= 10:
         break
     print(k)
     k += 1
+
+# Find if a number is there in a given list
+my_list = [2,4,5,6,8,9]
+number = 5
+index = 0
+count = 0
+while index < len(my_list):
+    if(my_list[index] == number):
+        count += 1
+        print("Number found at index: ", index)
+        break
+    index += 1
+if(count == 0):
+    print("Number not found")
+
+# Find if a number is there in a given tuple
+my_tuple = [2,4,5,6,8,9]
+number = 5
+index = 0
+count = 0
+while index < len(my_tuple):
+    if(my_tuple[index] == number):
+        count += 1
+        print("Number found at index: ", index)
+        break
+    index += 1
+if(count == 0):
+    print("Number not found")
