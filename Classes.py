d32cb59dd3a2df7e305ddf855132ab79e18a20af
prashdevplayground <prash.javadev@gmail.com>
2025-10-08 21:59:02 +0530

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -14,3 +14,34 @@ student3 = Student("Charlie", 23, "S54321")
 print(student1.display_info())
 print(student2.display_info())
 print(student3.display_info())
+
+
+class Employees:
+    def __init__(self, name, marks1, marks2, marks3):
+        self.name = name
+        self.marks1 = marks1
+        self.marks2 = marks2
+        self.marks3 = marks3
+
+    def average_marks(self):
+        return (self.marks1 + self.marks2 + self.marks3) /3
+    
+employee = Employees("David", 90, 85, 95)
+print(f"Employee Name: {employee.name}, Average Marks: {employee.average_marks()}")
+
+
+class Emp:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def average_marks(self):
+        return sum(self.marks) / len(self.marks)
+    
+    @staticmethod
+    def hello():
+        print("Hello from Emp class")
+    
+emp = Emp("David", [90, 85, 95])
+print(f"Employee Name: {emp.name}, Average Marks: {emp.average_marks()}")
+Emp.hello()
